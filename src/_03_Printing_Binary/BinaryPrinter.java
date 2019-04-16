e758bbbc460fff481e18b884e29e37e0fe268833
ilabstaff <ilabstaff@172.16.116.79>
2019-04-15 17:57:58 -0700

    Working on Binary Printer

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -5,12 +5,27 @@ public class BinaryPrinter {
 	//Don't be afraid to use the methods that are already complete to finish the others.
 	//Create a main method to test your methods.
 	
+	public static void main(String[] args) {
+		BinaryPrinter bp = new BinaryPrinter();
+		bp.printByteBinary((byte)3);
+		System.out.println("");
+		bp.printShortBinary((short)400);
+	}
+	
 	public void printByteBinary(byte b) {
-		
+		for (int i = 7; i > -1; i--) {
+			byte c = (byte) (b & (1 << i));
+			c >>= i;
+			System.out.print(Math.abs(c));
+			
+		}
 	}
 	
 	public void printShortBinary(short s) {
-		
+		byte c = (byte) (s & 0b000000011111111);
+		printByteBinary(c);
+		c = (byte) (s >>8);
+		printByteBinary(c);
 	}
 	
 	public void printIntBinary(int i) {
